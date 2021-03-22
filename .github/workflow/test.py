aeaed0aee8ed8b69e77afc977b307e23a938826f
MellisaNyengerai <81162997+MellisaNyengerai@users.noreply.github.com>
2021-03-22 18:48:45 +0000

    test.py

---
+++ .github/workflow/test.py
@@ -0,0 +1,20 @@
+print(len("hello world"))
+
+#finding a particular character in a string
+print("hello"[1])
+
+#Dot notation-object.method
+
+print("hello".upper())
+
+print("hello".lower())
+
+print("hello".capitalize())
+
+print("hello".count("l"))
+
+print("hello".find("l"))
+
+print("hello".replace("hello","HELLO"))
+
+print("hello".strip("h"))
